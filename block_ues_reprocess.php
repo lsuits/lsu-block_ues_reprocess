cfc3a4b40dd66fde5bccd652b61fc1b31e99c924
Philip Cali <philip.cali@gmail.com>
2012-01-17 08:29:47 -0600

    Better scaffolding

--- block_ues_reprocess.php
+++ block_ues_reprocess.php
@@ -5,12 +5,23 @@ class block_ues_reprocess extends block_list {
         $this->title = get_string('pluginname', 'ues_reprocess');
     }
 
+    function applicable_formats() {
+        return array('course' => true, 'my' => false, 'site' => false);
+    }
+
     function get_content() {
+
+        if ($this->content !== NULL) {
+            return $this->content;
+        }
+
         $content = new stdClass;
 
         $items = array();
         $icons = array();
 
+        $content->items = $items;
+        $content->icons = $icons;
         $content->footer = '';
 
         $this->content = $content;
